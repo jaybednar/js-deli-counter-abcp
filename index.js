29fd8b4f8ebdfc0d6614cb3b6fa3bd78e2450845
Jay Bednar <ironviralmedia@gmail.com>
2018-01-14 00:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,17 @@ function currentLine(line){
   if (line[0] === undefined) {
     return "The line is currently empty.";
   } else {
+<<<<<<< HEAD
     let deliLine = line.map(x => ` ${line.indexOf(x) + 1}. ${x}`);
     return `The line is currently:${deliLine}`;
+=======
+    let i = 0;
+    let current = line.splice(i);
+    while (i < line.length) {
+      current += current[i];
+      i++;
+    }
+    return `The line is currently: ${i + 1}. ${current}`;
+>>>>>>> 2b21f618d28a57db7b1a9b2bb935dacdd8cad2f0
   }
 }
